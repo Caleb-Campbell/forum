62b7edecc608b2ee5194589d773100452f536a2c
Caleb Campbell <calebcampbellcrm@gmail.com>
2022-12-29 13:29:51 -0800

    wip

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 
 // Import Routes
 const postRouter = require('./routes/postRoute')
 
 const server = express()
 
+server.use(cors)
 server.use(express.json())
 server.use('/api/posts', postRouter)
 
